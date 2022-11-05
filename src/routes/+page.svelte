64fb3fa105eb0a7e5a3a724d97d8c5c16aec5b5c
tristan <heytristaann@gmail.com>
2022-11-05 11:03:37 -0400

    cleaned up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,14 +31,14 @@
 	</div>
 	
 	<!-- "ENGAGE LIGHTSPEED" Button -->
-	<div class="lg:flex mt-6 xl:mt-8 justify-center items-center flex mr-8" out:fade={{ duration: 600 }}>
+	<div class="mt-6 justify-center items-center flex mr-8" out:fade={{ duration: 600 }}>
 		<button
 			on:click={async () => {
 				setTimeout(async () => ShowPlanets = true, window.innerHeight*3);
 				setTimeout(async () => ShowSimpsonGalaxyHeader = true, window.innerHeight*7)
 				EngageLightSpeed();
 			}}
-			class="text-center items-center justify-center flex text-2xl p-4 w-42 h-10 xl:w-42 xl:h-12 backdrop-blur-sm text-white rounded-lg font-black tracking-widest hover:bg-white hover:tracking-[0.2em] hover:text-black duration-700 ease-in-out">
+			class="px-4 text-xl md:text-2xl w-42 h-12 backdrop-blur-sm text-white rounded-lg font-black tracking-widest hover:bg-white hover:tracking-[0.2em] hover:text-black duration-700 ease-in-out">
 			ENGAGE LIGHT SPEED
 		</button>
 	</div>
