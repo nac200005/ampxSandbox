b007019344c62d7383b7cd77c3d72205dca339b5
tristan <heytristaann@gmail.com>
2022-11-06 14:17:24 -0500

    new build

---
+++ build/_app/immutable/chunks/1-fd403a4c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-3c31343f.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,9 +17,6 @@
 
 	// Header Management Variables
 	let ShowPlanets, ShowSimpsonGalaxyHeader = false;
-
-	// Get the screen hypoteneuse for better accuracy
-	const SCREEN_SIZE = Math.sqrt((window.innerHeight ** 2) + (window.innerWidth ** 2))
 </script>
 
 <!-- Main Header -->
@@ -37,8 +34,8 @@
 	<div class="mt-6 justify-center items-center flex mr-8" out:fade={{ duration: 600 }}>
 		<button
 			on:click={async () => {
-				setTimeout(async () => ShowPlanets = true, SCREEN_SIZE);
-				setTimeout(async () => ShowSimpsonGalaxyHeader = true, SCREEN_SIZE * 2.5)
+				setTimeout(async () => ShowPlanets = true, 2500);
+				setTimeout(async () => ShowSimpsonGalaxyHeader = true, 5300)
 				EngageLightSpeed();
 			}}
 			class="px-4 text-xl md:text-2xl w-42 h-12 backdrop-blur-sm text-white rounded-lg font-black tracking-widest hover:bg-white hover:tracking-[0.2em] hover:text-black duration-700 ease-in-out">
