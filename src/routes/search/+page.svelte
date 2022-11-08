2f01017d383e275b8ffeb21660c5a5746fa8b2f3
tristan <heytristaann@gmail.com>
2022-11-07 19:54:51 -0500

    changed search page main header

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -43,8 +43,15 @@
     });
 </script>
 
-<!-- Header -->
-<SimpsonGalaxyTitle/>
+<!-- "THE ... GALAXY" Title -->
+<div class="justify-center items-center flex mx-20" in:fade={{ delay: 400, duration: 1200 }}>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="mt-24 mb-10 font-black text-white tracking-widest text-6xl cursor-pointer hover:tracking-[0.25em] duration-500 ease-in-out" on:click={() => window.location.reload()}>
+        THE 
+        <mark style="background: none; color: #FF0055;">{SEARCH_WORD.toUpperCase()}</mark>
+        GALAXY
+    </div>
+</div>
 
 <!-- Github Repositories -->
 <div class="flex justify-center items-center"><div>
