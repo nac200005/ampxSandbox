03cc86af568502769c3d172c6fe53ddeb0d5f77a
tristan <heytristaann@gmail.com>
2023-03-31 19:34:31 -0400

    new /github page

---
+++ build/_app/immutable/modules/pages/github/_page.js-da39b3d8.js
@@ -0,0 +1,1 @@
+import{p as e,s as p}from"../../../chunks/_page-ec625255.js";export{e as prerender,p as ssr};
---
+++ src/routes/github/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        window.location = "https://github.com/orgs/Simpson-Computer-Technologies-Research/repositories"
+    })
+</script>
