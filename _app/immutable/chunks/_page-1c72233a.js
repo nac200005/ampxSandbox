e32e06869e2af58d4b96a36b7069213e41ef305f
Tristan Simpson <heytristaann@gmail.com>
2023-04-15 08:18:47 -0400

    Updates

--- _app/immutable/chunks/_page-1c72233a.js
+++
@@ -1,1 +0,0 @@
-const e=!0,r=!0,t=Object.freeze(Object.defineProperty({__proto__:null,ssr:!0,prerender:!0},Symbol.toStringTag,{value:"Module"}));export{t as _,r as p,e as s};
--- _app/immutable/modules/pages/github/_page.js-da39b3d8.js
+++
@@ -1,1 +0,0 @@
-import{p as e,s as p}from"../../../chunks/_page-ec625255.js";export{e as prerender,p as ssr};
